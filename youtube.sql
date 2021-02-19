a335b412a0386783b17cf8cfc0746b3a69144ba5
Patrick Chao <patrickchao55@gmail.com>
2021-02-18 19:57:57 -0800

    Add sql images and queries

--- youtube.sql
+++ youtube.sql
@@ -48,3 +48,50 @@ IGNORE 1 ROWS;
 
 # selecting to check that insertion were successful
 SELECT * FROM video_stats;
+
+-- With our tables successfully created with data inserted, we can query our data with questions such as:
+
+# What are the top 5 most liked videos?
+SELECT
+	title,
+    publishDate, 
+    likeCount
+FROM video_stats
+GROUP BY videoId
+ORDER BY likeCount DESC
+LIMIT 5;
+
+# Which videos have the highest dislike ratio?
+SELECT
+	title, 
+    CONCAT(ROUND(dislikeCount/(likeCount+dislikeCount) * 100, 2),'%') AS 'Dislike Ratio',
+    likeCount, 
+    dislikeCount
+FROM video_stats
+GROUP BY videoId
+ORDER BY 2 DESC
+LIMIT 3;
+
+# Which users have commented most often across all the videos from 3blue1brown?
+SELECT
+	authorId, 
+    authorName, 
+    COUNT(authorName) AS 'Number of Comments'
+FROM comments
+GROUP BY authorId
+ORDER BY COUNT(authorName) DESC
+LIMIT 10;
+
+# For the user who has commented the most, what were the top 5 videos that the user has commented on?
+SELECT
+	s.title,
+    c.authorName AS 'Commentor Name', 
+    COUNT(c.authorName) AS 'Number of Comments'
+FROM video_stats s
+INNER JOIN comments c
+	ON s.videoId = c.videoId
+WHERE authorId = 'UCGhQcael1605jqEQJQgIUJg'
+GROUP BY s.title
+ORDER BY COUNT(c.authorName) DESC
+LIMIT 5;
+
